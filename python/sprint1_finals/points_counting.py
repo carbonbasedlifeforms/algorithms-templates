bf753576bc2a3b529cc9d6daccb3bcaeded86780
Bikkulov Ravil <brh976@gmail.com>
2023-02-08 01:00:10 +0300

    fixed issues for points_counting.py

--- python/sprint1_finals/points_counting.py
+++ python/sprint1_finals/points_counting.py
@@ -1,23 +1,24 @@
 # 11 спринт, задача: Ловкость рук
 # id: 81925481
-
+from collections import Counter
 from typing import Tuple
 
-def points_counting(k: int, conc_str: str) -> int:
-    numbers = set(conc_str)
-    win_points = 0
-    for i in numbers:
-        if conc_str.count(i) <= k * 2:
-            win_points += 1
+def points_counting(taps: int, conc_str: str, gamers: int = 2) -> int:
+    keys = Counter(conc_str)
+    win_condition = taps * gamers
+    win_points = sum(
+        [1 for key, value in keys.items() if value <= win_condition and key.isdigit()]
+    )
     return win_points
 
-def read_input() -> Tuple[int, str]:
-    k = int(input())
-    i = 0
-    conc_str = ''
-    for i in range(4):
-        conc_str += str(input().replace('.',''))
-    return k, conc_str
-
-k, conc_str = read_input()
-print(points_counting(k, conc_str))
+if __name__ == '__main__':
+   
+    def read_input() -> Tuple[int, str]:
+        taps = int(input())
+        conc_str = ''
+        for str_num in range(4):
+            conc_str += str(input())
+        return taps, conc_str
+    
+    taps, conc_str = read_input()
+    print(points_counting(taps, conc_str))
