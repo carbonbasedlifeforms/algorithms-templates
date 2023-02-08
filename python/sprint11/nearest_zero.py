bcde46ed2305d8049933c8738a0c987ce19cf937
Bikkulov Ravil <brh976@gmail.com>
2023-02-08 21:18:20 +0300

    sprint 11 second attempt

--- python/sprint11/nearest_zero.py
+++ python/sprint11/nearest_zero.py
@@ -1,15 +1,12 @@
 # 11 спринт, задача: Ближайший ноль
-# id: 81971250
-
+# id: 82012806
 from typing import List, Tuple
+
 
 def nearest_zero(elements_qty: int, input_struct: List[int]) -> List[int]:
     distances = [0] * elements_qty
-    empty_sites = []
 
-    for idx, value in enumerate(input_struct):
-        if value == 0:
-            empty_sites.append(idx)
+    empty_sites = [idx for idx, value in enumerate(input_struct) if value == 0]
 
     for idx in range(empty_sites[0]):
         distances[idx] = empty_sites[0] - idx
@@ -19,13 +16,19 @@ def nearest_zero(elements_qty: int, input_struct: List[int]) -> List[int]:
 
     for znum in range(len(empty_sites)-1):
         for idx in range(empty_sites[znum] + 1, empty_sites[znum + 1]):
-            distances[idx] = min(idx - empty_sites[znum], empty_sites[znum + 1] - idx)
+            distances[idx] = min(
+                idx - empty_sites[znum],
+                empty_sites[znum + 1] - idx
+                )
     return distances
 
-def read_input() -> Tuple[int, List[int]]:
-    elements_qty = int(input())
-    input_struct = list(map(int, input().strip().split()))
-    return elements_qty, input_struct
 
-elements_qty, input_struct = read_input()
-print(" ".join(map(str, nearest_zero(elements_qty, input_struct))))
+if __name__ == '__main__':
+
+    def read_input() -> Tuple[int, List[int]]:
+        elements_qty = int(input())
+        input_struct = list(map(int, input().strip().split()))
+        return elements_qty, input_struct
+
+    elements_qty, input_struct = read_input()
+    print(" ".join(map(str, nearest_zero(elements_qty, input_struct))))
--- python/sprint11/points_counting.py
+++ python/sprint11/points_counting.py
@@ -1,24 +1,26 @@
 # 11 спринт, задача: Ловкость рук
-# id: 81925481
+# id: 82012855
 from collections import Counter
 from typing import Tuple
 
+
 def points_counting(taps: int, conc_str: str, gamers: int = 2) -> int:
-    keys = Counter(conc_str)
+    keys = Counter(conc_str.replace('.', ''))
     win_condition = taps * gamers
     win_points = sum(
-        [1 for key, value in keys.items() if value <= win_condition and key.isdigit()]
+        [value <= win_condition for key, value in keys.items()]
     )
     return win_points
 
+
 if __name__ == '__main__':
-   
+
     def read_input() -> Tuple[int, str]:
         taps = int(input())
         conc_str = ''
         for str_num in range(4):
             conc_str += str(input())
         return taps, conc_str
-    
+
     taps, conc_str = read_input()
     print(points_counting(taps, conc_str))
