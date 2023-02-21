b504f4693a0f23f5676a0e5ef2dcaefb9f38af9e
Bikkulov Ravil <brh976@gmail.com>
2023-02-21 23:31:56 +0300

    sprint 12 rpn calc fixed review issues

--- python/sprint12/rpn_calculator.py
+++ python/sprint12/rpn_calculator.py
@@ -1,5 +1,5 @@
 # 12 sprint, Калькулятор
-# id: 82711514
+# id: 82764129
 from operator import add, floordiv, mul, sub
 from typing import List, Dict
 
@@ -12,24 +12,21 @@ class Stack:
         self.result.append(value)
 
     def pop(self):
-        if self.result == []:
-            raise IndexError()
+        if not self.result:
+            exit(0)
         return self.result.pop()
 
 
-def rpn_calculator(expr: List, operators: Dict) -> int:
+def rpn_calculator(expr: List[str], operators: Dict[str, str]) -> int:
     characters = Stack()
-    try:
-        for element in expr:
-            if element in operators:
-                operand1, operand2 = characters.pop(), characters.pop()
-                atom_calc = operators[element](operand2, operand1)
-                characters.push(atom_calc)
-            else:
-                characters.push(int(element))
-        print(characters.pop())
-    except IndexError:
-        pass
+    for element in expr:
+        if element in operators:
+            operand1, operand2 = characters.pop(), characters.pop()
+            atom_calc = operators[element](operand2, operand1)
+            characters.push(atom_calc)
+        else:
+            characters.push(int(element))
+    print(characters.pop())
 
 
 if __name__ == '__main__':
